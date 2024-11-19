745176041663148a8fc6ab5825324a2e444a2a36
carrowkeel <6824521+carrowkeel@users.noreply.github.com>
2024-11-19 09:40:51 +0200

    fixed problem with axes and added plots

--- plot.js
+++ plot.js
@@ -49,13 +49,18 @@ export const createCanvas = (container, bounds=[[0, 1], [0, 1]], ratio=1) => {
 
 export const initializePlots = (container, plots) => {
 	plots.forEach(plot => {
+		if (draw_plot[plot.type] === undefined)
+			throw `The plot type ${plot.type} is not defined`;
 		const plot_elem = document.createElement('div');
 		plot_elem.classList.add('plot');
+		plot_elem.dataset.save = plot.save || 'dynamics';
 		plot_elem.dataset.plot = plot.label;
-		const legend = (plot.legend !== undefined ? plot.legend : plot.y || plot.data).split(',').filter(entry => entry !== '').map(legend_entry => `<a>${mathNotation(legend_entry)}</a>`).join('');
-		plot_elem.innerHTML = `<div class="crosshair"></div><div class="header"><a data-icon="e" data-action="export" class="settings fright"></a><div class="title">${plot.label}</div></div><div class="legend">${legend}</div><div class="draw_area"><div class="axis-label label-x">${mathNotation(plot.xlabel)}</div><div class="axis-label label-y">${mathNotation(plot.ylabel)}</div><div class="axis-tick xmin">${shortNotation(plot.xlim[0])}</div><div class="axis-tick ymin editable" title="Edit y-axis">${shortNotation(plot.ylim[0])}</div><div class="axis-tick xmax">${shortNotation(plot.xlim[1])}</div><div class="axis-tick ymax editable" title="Edit y-axis">${shortNotation(plot.ylim[1])}</div></div></div>`;
+		plot_elem.dataset.plotType = plot.type;
+		const legend = (plot.legend !== undefined ? plot.legend : plot.y || plot.data || '').split(',').filter(entry => entry !== '').map(legend_entry => `<a>${mathNotation(legend_entry)}</a>`).join('');
+		plot_elem.innerHTML = `<div class="crosshair"></div><div class="header"><a data-icon="e" data-action="export" class="settings fright"></a><div class="title">${plot.label || 'dynamics'}</div></div><div class="legend">${legend}</div><div class="draw_area"><div class="axis-label label-x">${mathNotation(plot.xlabel || 'x')}</div><div class="axis-label label-y">${mathNotation(plot.ylabel || 'y')}</div><div class="axis-tick xmin">${shortNotation(plot.xlim[0])}</div><div class="axis-tick ymin">${shortNotation(plot.ylim[0])}</div><div class="axis-tick xmax">${shortNotation(plot.xlim[1])}</div><div class="axis-tick ymax">${shortNotation(plot.ylim[1])}</div></div></div>`;
 		container.appendChild(plot_elem);
-		createCanvas(plot_elem.querySelector('.draw_area'));
+		if (plot.type !== 'image')
+			createCanvas(plot_elem.querySelector('.draw_area'));
 	});
 };
 
@@ -64,6 +69,16 @@ export const updatePlots = (container, plots, data, update_axis=false) => {
 		const plot_elem = container.querySelector(`[data-plot="${plot.label}"]`);
 		if (!plot_elem)
 			return;
+		if (draw_plot[plot.type] === undefined)
+			throw `The plot type ${plot.type} is not defined`;
+		if (plot.type === 'image') {
+			data.forEach(image_uri => {
+				const img = document.createElement('img');
+				img.setAttribute('src', image_uri)
+				plot_elem.querySelector('.draw_area').appendChild(img);
+			});
+			return;
+		}
 		const canvas = plot_elem.querySelector('canvas');
 		const draw_elem = canvas_draw(canvas, canvas.getContext('2d'), [plot.xlim, plot.ylim]);
 		if (update_axis === true) {
@@ -77,16 +92,27 @@ export const updatePlots = (container, plots, data, update_axis=false) => {
 			case plot.y !== undefined:
 				plot.y.split(',').forEach((y, i) => {
 					const x = plot.x.split(',').length === 1 ? plot.x : plot.x.split(',')[i];
+					if (data[0][x] === undefined)
+						throw `Failed to draw plot ${plot.label}: x-axis parameter ${x} not found in model output`;
+					if (data[0][y] === undefined)
+						throw `Failed to draw plot ${plot.label}: y-axis parameter ${y} not found in model output`;
 					const plot_data = data.map(data_point => [x, y].map(prop => data_point[prop]));
 					draw_plot[plot.type](draw_elem, plot_data, i);
 				});
 				break;
 			case plot.data !== undefined:
-				const plot_data = plot.data.split(',').map(prop => data[data.length - 1][prop]);
+				const data_parameters = plot.data.split(',');
+				for (const data_parameter of data_parameters)
+					if (data[0][data_parameter] === undefined)
+						throw `Parameter ${data_parameter} not found in model output`;
+				const plot_data = data_parameters.map(prop => data[data.length - 1][prop]);
 				draw_plot[plot.type](draw_elem, plot_data, 0);
 				break;
-			default:
+			case plot.function !== undefined:
 				draw_plot[plot.type](draw_elem, data);
+				break;
+			default:
+				throw `Failed to draw plot ${plot.label}: plot input not defined in scheme (use 'data' or 'y' to define the input of the plot)`;
 		}
 	});
 };
@@ -99,27 +125,36 @@ export const draw_plot = {
 	line: (draw, line, i) => {
 		draw.line_x(line, color_cycle[i]);
 	},
+	scatter_plot: (draw, points, i, r=5, opacity=0.5) => {
+		points.forEach(point => draw.point(point, color_cycle[i], r, point[2] !== undefined ? point[2] : opacity));
+	},
 	lines: (draw, lines, i) => {
 		const lines_mean = lines.map(([x, line_y]) => [x, mean(line_y)]);
 		const lines_rep = range(0, lines[0][1].length).map(i => lines.map(([x, line_y]) => [x, line_y[i]]));
 		draw.line_x(lines_mean, color_cycle[i]);
 		lines_rep.map(line => draw.line_x(line, color_cycle[i], 0.2));
 	},
+	grayscale_2d: (draw, data, i) => {
+		draw.pre();
+		data.forEach((row, y) => row.forEach((cell, x) => {
+			const color = [255 * cell, 255 * cell, 255 * cell];
+			draw.normed_rect([x / data[0].length, (data.length - y - 1) / data.length, 1 / data[0].length, 1 / data.length], color);
+		}));
+		draw.post();
+	},
 	mat: (draw, data, i) => {
 		draw.pre();
-		data.forEach((row, y) => {
-			row.forEach((cell, x) => {
-				const color = Array.isArray(cell) ? cell : (typeof cell === 'number' ? [255 * cell, 255 * (1 - cell), 0]: [200, 200, 200]);
-				draw.square([x / row.length, y / data.length, 1 / row.length, 1 / data.length], color);
-			});
-		});
+		data.forEach((row, y) => row.forEach((cell, x) => {
+			const color = Array.isArray(cell) ? cell : (typeof cell === 'number' ? [255 * cell, 255 * (1 - cell), 0] : [200, 200, 200]);
+			draw.normed_rect([x / data[0].length, (data.length - y - 1) / data.length, 1 / data[0].length, 1 / data.length], color);
+		}));
 		draw.post();
 	},
 	grid: (draw, data, i) => {
 		draw.pre();
 		data[0].forEach(([x, y, value]) => {
 			const color = Array.isArray(value) ? value : (typeof value === 'number' ? [255 * value, 255 * (1 - value), 0]: [200, 200, 200]);
-			draw.square([x, y, 1, 1], color);
+			draw.scaled_rect([x, y, 1, 1], color);
 		});
 		draw.post();
 	},
@@ -132,6 +167,8 @@ export const draw_plot = {
 			const color = range(0, 3).map(rgb_i => color_cycle[i][rgb_i] + (color_cycle[i + 1][rgb_i] - color_cycle[i][rgb_i]) * node[2]);
 			draw.point(node.slice(0, 2), color);
 		}
+	},
+	image: (draw, data, i) => {
 	},
 };
 
@@ -157,7 +194,17 @@ export const svg_draw = (svg, bounds=[[0, 1], [0, 1]], dims=[svg.dataset.width, 
 	line: function (line, color, dynamic=true, offset=0, opacity=1) {
 		this.polyline(line.map((y,x)=>[(offset+x-bounds[0][0])*scale[0], (bounds[1][1]-y)*scale[1]]), color, opacity, dynamic ? 'dynamic' : '', {'data-value': line.map(v => round(v, 10)).join(',')});
 	},
-	square: function (s, color, opacity=1) {
+	normed_rect: function (s, color, opacity=1) {
+		const [x, y] = [s[0] * dims[0], s[1] * dims[1]];
+		const rect = [
+			x,
+			y,
+			dims[0] * s[2],
+			dims[1] * s[3]
+		].map(v => round(v, 4));
+		this.rect(...rect, color, opacity);
+	},
+	scaled_rect: function (s, color, opacity=1) {
 		const [x, y] = [scale[0] * (s[0] - bounds[0][0]), scale[1] * (bounds[1][1]-s[1]-s[3])];
 		const rect = [
 			x,
@@ -236,7 +283,17 @@ export const canvas_draw = (canvas, ctx, bounds=[[0, 1], [0, 1]], dims=[canvas.d
 		else
 			this.polyline(line.map((y,x)=>[(offset+x-bounds[0][0])*scale[0], (bounds[1][1]-y)*scale[1]]), color, opacity);
 	},
-	square: function (s, color, opacity=1, stroke=true) {
+	normed_rect: function (s, color, opacity=1) {
+		const [x, y] = [s[0] * dims[0], s[1] * dims[1]];
+		const rect = [
+			x,
+			y,
+			dims[0] * s[2],
+			dims[1] * s[3]
+		].map(v => round(v, 4));
+		this.rect(...rect, color, opacity);
+	},
+	scaled_rect: function (s, color, opacity=1, stroke=true) {
 		const [x, y] = [scale[0] * (s[0] - bounds[0][0]), scale[1] * (bounds[1][1]-s[1]-s[3])];
 		const rect = [
 			x,
